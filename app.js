b8b7a7c29483d2c45ab14383c099139d3a809fab
eclipman0 <eclipman0@gmail.com>
2025-01-29 23:55:17 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json({ type: ['application/json', 'application/csp-report'] }));
+
 app.get('/', (req, res) => {
     const hubChallenge = req.query['hub.challenge'];
     res.send(hubChallenge);
     // res.send('Hello World!')
-})
+});
+
+app.post('/webhooks', (req, res) => {
+    res.status(200).send(req.body.object);
+    console.log(req.body);
+    
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
